9d4b314838c68aeaeb493bfffc8908484d1aa953
Josh Norman <J.Norman2@hull.ac.uk>
2025-05-30 17:19:38 +0100

    Blank module

--- shiny/modules/deploymentModule.R
+++ shiny/modules/deploymentModule.R
@@ -6,18 +6,11 @@ deploymentUI <- function(id) {
     h4("Blank"),
     #Main panel UI here
     fluidRow(
-      column(8, h3("Add Deployment Information"),
-             p("This tab will allow users to add and edit deployment information for sensors.")), 
-      
-      hr(),
-      
       column(12, h3("Add Deployment Information"),
              p("This tab will allow users to add and edit deployment information for sensors.")),
       
       hr(),
-      
-      column(4, h3("Add Deployment Information"),
-             p("This tab will allow users to add and edit deployment information for sensors."))
+
     )
   )
 }
@@ -42,11 +35,34 @@ deploymentServer <- function(id, raw_data_path, output_dir, processing_complete)
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
-    # Add actual deployment logic here
+    
+    
+    # ============================= #
+    # /// Reactive values \\\ ####  
+    # ============================= #   
+    
+    
+    
+    # ============================= #
+    # /// Event handlers \\\ ####  
+    # ============================= # 
+    
+##header ####
     observeEvent(input$add_deploy_btn, {
       # Handle deployment info addition
       showNotification("Deployment info functionality coming soon!", type = "message")
     })
+    
+    # ============================= #
+    # /// Helper functions \\\ ####  
+    # ============================= # 
+    
+    
+    
+    
+    # ============================= #
+    # /// Output render \\\ ####  
+    # ============================= #   
     
     # Return any reactive values other modules might need
     return(list())
